d2e9a3e61202f29537e6465bef10e4f672b424ab
LORD SEIDON <lodianaselora@gmail.com>
2022-09-19 11:42:29 +0100

    Lord

--- schoolApp/frontend/src/components/header/Header.jsx
+++ schoolApp/frontend/src/components/header/Header.jsx
@@ -12,8 +12,7 @@ const Header = () => {
           <div className="header-left">
             <h1>Securing An Excellent Future For Your Kids</h1>
             <p className="sub-text-h">Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.</p>
-            <div className="sign-h">School Portal</div>
-
+            <div className="sign-h"><a href='/portal' className='white'>School Portal</a> </div>
           </div>
           <div className="header-right"></div>
         </div> 
